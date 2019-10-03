9b53519089f22a75c540ff61856ba82d49e59c80
tamas.markovics <markovics.ttamas@gmail.com>
2019-10-03 15:55:14 +0200

    handle some argument errors, style corrections

--- toDoApp/main.cpp
+++ toDoApp/main.cpp
@@ -3,10 +3,8 @@
 #include <vector>
 using std::string;
 using std::cout;
-using std::cin;
 using std::endl;
 using std::vector;
-
 #include <fstream>
 
 void instructions();
@@ -16,23 +14,30 @@ void addNewTask(string whatToAdd);
 void removeTask(string num);
 vector<string> createVector();
 void taskDone(string num);
+void printOutVector(vector<string> toPrint);
 
 int main(int argc, char* argv[])
 {
     cout << endl;
-
     if (argv[1] == string("todo")){
         if (argc == 2){
             instructions();
+
         } else if (string(argv[2]) == "-l"){
             listTasks();
+
         } else if (string(argv[2]) == "-a") {
             if (argc == 4) addNewTask(argv[3]);
             else cout << "No new tasks to add!" << endl;
+
         } else if (string(argv[2]) == "-r") {
-                removeTask(argv[3]);
+            if (argc == 4) removeTask(argv[3]);
+            else cout << "No task to remove!" << endl;
+
         }else if (string(argv[2]) == "-c") {
-            taskDone(argv[3]);
+            if (argc == 4) taskDone(argv[3]);
+            else cout << "No task to be completed!" << endl;
+
         }
     }
 
@@ -42,15 +47,13 @@ int main(int argc, char* argv[])
 
 void instructions()
 {
-
-    cout << "Command Line Todo application\n";
-    cout << "=============================\n\n";
-    cout << "Command line arguments:\n";
-    cout <<     "\t-l   Lists all the tasks\n";
-    cout <<     "\t-a   Adds a new task\n";
-    cout <<     "\t-r   Removes a task\n";
-    cout <<     "\t-c   Completes a task\n";
-
+    cout << "Command Line Todo application\n"
+            "=============================\n\n"
+            "Command line arguments:\n"
+                "\t-l   Lists all the tasks\n"
+                "\t-a   Adds a new task\n"
+                "\t-r   Removes a task\n"
+                "\t-c   Completes a task\n";
 }
 
 int countLines()
@@ -99,7 +102,6 @@ void addNewTask(string whatToAdd)
     cout << "New task added!" << endl;
 }
 
-
 void removeTask(string num)
 {
     if(std::stoi(num) <= countLines()) {
@@ -107,13 +109,7 @@ void removeTask(string num)
 
         myVector.erase(myVector.begin() + std::stoi(num) - 1);
 
-        std::ofstream myFile;
-        myFile.open("toDoList.txt");
-        for (int i = 0; i < myVector.size() - 1; i++) {
-            myFile << myVector[i] << endl;
-        }
-        myFile << myVector[myVector.size() - 1];
-        myFile.close();
+        printOutVector(myVector);
 
         cout << "Task removed!" << endl;
     } else cout << "There isn't that many tasks!" << endl;
@@ -124,32 +120,39 @@ void taskDone(string num)
     if(std::stoi(num) <= countLines()) {
         vector<string> myVector = createVector();
 
-        myVector[std::stoi(num)][1] = 'X';
+        myVector[std::stoi(num) - 1][1] = 'X';
 
-        std::ofstream myFile;
-        myFile.open("toDoList.txt");
-        for (int i = 0; i < myVector.size() - 1; i++) {
-            myFile << myVector[i] << endl;
-        }
-        myFile << myVector[myVector.size() - 1];
-        myFile.close();
+        printOutVector(myVector);
 
         cout << "Task completed!" << endl;
     } else cout << "There isn't that many tasks!" << endl;
 }
-
 
 vector<string> createVector()
 {
     vector<string> myVector;
     int lineCounter = countLines();
     std::string lines;
+
     std::ifstream inputFile;
     inputFile.open("toDoList.txt");
+
     for (int i = 0; i < lineCounter; ++i) {
         std::getline(inputFile, lines);
         myVector.push_back(lines);
     }
+
     inputFile.close();
     return myVector;
+}
+
+void printOutVector(vector<string> toPrint)
+{
+    std::ofstream myFile;
+    myFile.open("toDoList.txt");
+    for (int i = 0; i < toPrint.size() - 1; i++) {
+        myFile << toPrint[i] << endl;
+    }
+    myFile << toPrint[toPrint.size() - 1];
+    myFile.close();
 }
